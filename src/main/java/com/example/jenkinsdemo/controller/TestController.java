e243827ddb146a87116587c5cea5ce888a6b6c56
honeyland414 <wangkai.honeyland@gmail.com>
2023-03-01 23:35:39 -0500

    first commit

---
+++ src/main/java/com/example/jenkinsdemo/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/myJenkins")
+    public String hello() {
+        return "hello jenkins!!";
+    }
+}
